b58e84001d3931ff39dcc1f6b910869b52c32287
Jonathan A. Kollasch <jakllsch@kollasch.net>
2016-07-05 10:22:36 -0500

    local_stage0.sh: recognize NetBSD

--- src/etc/local_stage0.sh
+++ src/etc/local_stage0.sh
@@ -18,7 +18,7 @@ LIB_PREFIX=lib
 
 OS=`uname -s`
 case $OS in
-    ("Linux"|"FreeBSD"|"DragonFly"|"Bitrig"|"OpenBSD"|"SunOS")
+    ("Linux"|"FreeBSD"|"DragonFly"|"NetBSD"|"Bitrig"|"OpenBSD"|"SunOS")
     BIN_SUF=
     LIB_SUF=.so
     ;;
