5095cbec70c0900912882b9260493d7f24a57e86
Jonathan A. Kollasch <jakllsch@kollasch.net>
2017-08-30 18:12:05 -0500

    Add armv7-unknown-netbsd-eabihf target

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -429,6 +429,7 @@ impl Step for Openssl {
             "arm-unknown-linux-gnueabihf" => "linux-armv4",
             "armv7-linux-androideabi" => "android-armv7",
             "armv7-unknown-linux-gnueabihf" => "linux-armv4",
+            "armv7-unknown-netbsd-eabihf" => "BSD-generic32",
             "i686-apple-darwin" => "darwin-i386-cc",
             "i686-linux-android" => "android-x86",
             "i686-unknown-freebsd" => "BSD-x86-elf",
